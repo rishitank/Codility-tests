c427fd4d6d8f5e1c1f937e74acf3cf79a04537e3
Rishi Tank <tank.rishi@gmail.com>
2016-09-06 17:45:12 +0100

    Improvement to CyclicRotation solution

--- java/CyclicRotation.java
+++ java/CyclicRotation.java
@@ -1,9 +1,9 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int[] solution(int[] A, int K) {
-        List<Integer> arrayList = new ArrayList<Integer>(A.length);
-        for (int i : A) arrayList.add(i);
+        List<Integer> arrayList = Arrays.stream(A).boxed().collect(Collectors.toList());
         Collections.rotate(arrayList, K);
         
         return arrayList.stream().mapToInt(i -> i).toArray();
